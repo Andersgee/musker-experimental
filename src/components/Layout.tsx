3ba17c8c1a81cf365346ed1a48af4bd39d222c89
Anders Gustafsson <andersgee@gmail.com>
2022-11-04 00:38:44 +0100

    fix typo, also might want to make aside sticky?

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -24,26 +24,6 @@ export function Layout({ children, className }: Props) {
   );
 }
 
-export function LayoutHmm({ children, className }: Props) {
-  return (
-    <div
-      className="container grid h-screen 
-    grid-rows-phone sm:grid-cols-sm
-    sm:grid-rows-sm md:grid-cols-md
-    md:grid-rows-md lg:grid-cols-lg
-    lg:grid-rows-lg
-    "
-    >
-      <Header className="flex h-12 items-center justify-between bg-red-500 sm:col-span-1 sm:col-start-2" />
-      <main className="overflow-y-scroll bg-green-500 sm:col-span-1 sm:col-start-2 sm:bg-cyan-500 md:bg-purple-500 lg:bg-green-500">
-        {children}
-      </main>
-      <Nav className="h-12 bg-orange-500 sm:col-span-1 sm:col-start-1 sm:row-span-2 sm:row-start-1 sm:h-full" />
-      <aside className="hidden md:col-start-3 md:row-start-1 md:block">aside</aside>
-    </div>
-  );
-}
-
 function Header({ className }: { className?: string }) {
   const { data: sessionData } = useSession();
   return (
