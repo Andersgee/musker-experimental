3962d51536e83d62b219f81766d03d91625be2c0
Anders Gustafsson <andersgee@gmail.com>
2022-11-03 02:53:35 +0100

    IconMusker animated

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -1,7 +1,14 @@
+import { IconMusker } from "src/icons/Musker";
+
 type Props = {
   className?: string;
 };
 
 export default function Messages({ className }: Props) {
-  return <div className={className}>messages</div>;
+  return (
+    <div className={className}>
+      <div>messages</div>
+      <IconMusker className="w-full" />
+    </div>
+  );
 }
