f1516286b3f56e78410222d3ad61e74d46b5b5ae
Anders Gustafsson <andersgee@gmail.com>
2022-11-12 01:27:34 +0100

    animate smoke on Musker svg

--- src/app/u/[handle]/followers/page.tsx
+++ src/app/u/[handle]/followers/page.tsx
@@ -2,14 +2,16 @@
 
 //import {useRouter} from "next/navigation"
 import { usePathname } from "next/navigation";
+import { IconMusker } from "src/icons/Musker";
 
 export default function Page() {
   const pathname = usePathname();
   //const name = pathname?.split("/").at(-1);
   return (
     <div className="">
-      <div>followers page</div>
-      <div>pathname: {pathname}</div>
+      <div>{pathname}</div>
+      <div>TODO</div>
+      <IconMusker className="w-full" />
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -35,6 +35,7 @@ module.exports = {
         strokedraw: "strokedraw 2.5s cubic-bezier(0.33, 0.4, 0.96, 0.6) forwards",
         slowstrokedraw: "strokedraw 10s cubic-bezier(0.33, 0.4, 0.96, 0.6) forwards",
         muskstrokedraw: "strokedraw 1s cubic-bezier(.17,.67,.72,.78) forwards",
+        muskstrokedrawslowlinear: "strokedraw 3s cubic-bezier(0,0,0,0) forwards infinite",
       },
       boxShadow: {
         imageborder: "inset 0 2px 4px 0 hsla(0, 0%, 0%, .2)",
