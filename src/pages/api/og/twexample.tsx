a83b0611c5de313eda0b8368aae8516f51e7ecf0
Anders Gustafsson <andersgee@gmail.com>
2022-11-21 18:48:14 +0100

    kind of okay

--- src/pages/api/og/twexample.tsx
+++ src/pages/api/og/twexample.tsx
@@ -7,7 +7,8 @@ export const config = {
 export default async function handler() {
   //anders
   const image = "https://lh3.googleusercontent.com/a/ALm5wu1o8vscH0Z-lGyE8x2IPSs6lsFT91u-_7S1GlzBlw=s96-c";
-
+  const handle = "andy";
+  const datestr = "Nov 21";
   return new ImageResponse(
     (
       // Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
@@ -19,23 +20,25 @@ export default async function handler() {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          backgroundColor: "white",
+          backgroundColor: "#FAFAFA",
         }}
       >
         <div tw="flex">
           <img tw="h-16 w-16 rounded-full mt-4" src={image} />
-          <div tw="flex flex-col">
-            <h3 tw="text-xl font-normal">username here - datestr here</h3>
+          <div tw="flex flex-col pl-4 pt-4">
+            <h3 tw="text-2xl font-normal p-0 m-0">
+              {handle} - {datestr}
+            </h3>
             <p tw="text-lg max-w-lg">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quae est dolore explicabo dignissimos
-              tempore quisquam atque nesciunt repellendus ex. Libero ab atque animi quidem? Adipisci, architecto.
-              Voluptas, nesciunt qui?
+              tempore quisquam atque nesciunt repellendus ex.
             </p>
-            <div tw="flex gap-4">
-              <div>replyicon</div>
-              <div>retweeticon</div>
-              <div>hearticon</div>
+            <div tw="flex">
+              <div tw="mr-2">replyicon</div>
+              <div tw="mr-2">retweeticon</div>
+              <div tw="mr-2">hearticon</div>
             </div>
+            <div tw="my-2 w-full border-b-2 border-gray-200"></div>
           </div>
         </div>
       </div>
