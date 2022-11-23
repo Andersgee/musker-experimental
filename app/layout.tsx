b9c0effb6a741502069481863a20cb35ec423df4
Anders Gustafsson <andersgee@gmail.com>
2022-11-23 22:16:58 +0100

    move rootlayout (avoid empty fragment)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import "src/styles/globals.css";
 
-import { LayoutRSC } from "src/components/LayoutRSC";
+//import { LayoutRSC } from "src/components/LayoutRSC";
 import { TrpcProvider } from "src/contexts/TrpcContext";
 import { SessionProvider } from "src/contexts/SessionContext";
 import { ThemeProvider } from "src/contexts/next-themes";
 import { Montserrat } from "@next/font/google";
 import { DialogProvider } from "src/contexts/Dialog";
+import { SignInDialog } from "src/components/SignInDialog";
+import { NavRSC } from "src/components/NavRSC";
+import { HeaderRSC } from "src/components/HeaderRSC";
 
 //const montserrat = Montserrat({ weight: "variable", subsets: ["latin"] });
 
@@ -21,7 +24,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <ThemeProvider>
             <DialogProvider>
               <body>
-                <LayoutRSC>{children}</LayoutRSC>
+                <SignInDialog />
+                <div className="container grid grid-rows-phone sm:grid-cols-sm sm:grid-rows-sm md:grid-cols-md md:grid-rows-md lg:grid-cols-lg lg:grid-rows-lg">
+                  <NavRSC className="fixed bottom-0 h-12 w-full border-t sm:h-full sm:w-16 sm:border-t-0 lg:w-72" />
+                  <HeaderRSC className="headercontainer fixed h-12 sm:ml-16 lg:ml-72" />
+                  <main className="headercontainer row-start-2 sm:col-start-2">{children}</main>
+                  <aside className="hidden  md:col-start-3 md:row-span-2 md:row-start-1 md:block">
+                    <article className="mx-2 mt-1">
+                      <h2>What is this?</h2>
+                      <p>
+                        Musker is a twitter clone built with the latest (experimental) features of nextjs 13 such as
+                        server components and more.
+                      </p>
+                    </article>
+                  </aside>
+                </div>
               </body>
             </DialogProvider>
           </ThemeProvider>
